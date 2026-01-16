4d60e4b0c82c09af9779432aff8af677ee952dc7
Scala Steward <scala_steward@virtuslab.com>
2026-01-16 00:10:14 +0000

    Update smithy-build to 1.66.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 object Dependencies {
 
   object Smithy {
-    val version = "1.58.0"
+    val version = "1.66.0"
     val build   = "software.amazon.smithy" % "smithy-build" % version
   }
 }
